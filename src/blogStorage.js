11ef6c159e9cb3f32db5ffa9e60da61b792c6d1b
Eva-Li80 <evaliboden@gmail.com>
2023-10-18 09:12:49 +0200

    update

--- src/blogStorage.js
+++ src/blogStorage.js
@@ -1,21 +1,26 @@
 export const saveBlogPosts = (posts) => {
-    localStorage.setItem('blogPosts', JSON.stringify(posts));
-  };
-  
-  export const getBlogPosts = () => {
-    const storedPosts = localStorage.getItem('blogPosts');
-    return storedPosts ? JSON.parse(storedPosts) : [];
-  };
+  localStorage.setItem('blogPosts', JSON.stringify(posts));
+};
 
-  const initialBlogPosts = [
-    {
-      id: 1,
-      title: "Welcome to the blog",
-      body: "Try to post something 🥰",
-      author: "Admin",
-    },
-  ];
-  
-  if (!localStorage.getItem('blogPosts')) {
-    saveBlogPosts(initialBlogPosts);
-  }
+export const getBlogPosts = () => {
+  const storedPosts = localStorage.getItem('blogPosts');
+  return storedPosts ? JSON.parse(storedPosts) : [];
+};
+
+const initialBlogPosts = [
+  {
+    id: 1,
+    title: "Welcome to the family blog",
+    body: "Try to post something 🥰",
+    author: "Admin",
+  },
+];
+
+const existingPosts = getBlogPosts();
+
+if (existingPosts.length === 0) {
+  saveBlogPosts(initialBlogPosts);
+}
+
+
+
